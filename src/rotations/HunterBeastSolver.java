df956e9db6e3b6579fb8b6ef05732808dd628c42
TheCrux <b.mail93@web.de>
2017-10-23 18:26:27 +0200

    Added mount support

--- src/rotations/HunterBeastSolver.java
+++ src/rotations/HunterBeastSolver.java
@@ -58,7 +58,32 @@ public class HunterBeastSolver implements RotationSolver {
   int mDrinkingBuffs[] = { 430, 431, 432, 1133, 1135, 1137, 10250, 22734, 24355, 29007 };
 
   // Eating Buffs
-  int mEatingBuffs[] = { 433, 434, 1127, 1129, 1131, 25700, 25886, 28616, 29008, 29073 };
+  int mEatingBuffs[] = { 433, 434, 435, 1127, 1129, 1131, 2639, 6410, 7737, 24005, 25700, 25886, 28616, 29008, 29073 };
+
+  // Mounts
+  int mMounts[] = {
+      // Wolfs
+      1132, 6653, 6654, 23251, 23252, 23250,
+      // Raptors
+      10799, 10796, 8395, 23243, 23242, 23241,
+      // Kodos
+      18989, 18990, 23247, 23248, 23249,
+      // Undead Horses
+      17462, 17464, 17463, 17465, 23246,
+      // Horses
+      472, 6648, 458, 470, 23228, 23227, 23229,
+      // Saber
+      10789, 8394, 10793, 23221, 23219, 23338,
+      // Rams
+      6898, 6777, 6899, 23240, 23239, 23238,
+      // Mechanostrider
+      17454, 10873, 17453, 10969, 23222, 23223, 23225,
+      // PvP - Horde
+      22721, 22722, 22724, 22718, 23509,
+      // PvP - Alliance
+      22717, 22723, 22720, 22719, 23510,
+      // Special (mostly Dungeons)
+      24252, 17450, 24242, 16084, 18991, 18992, 17229 };
 
   public HunterBeastSolver(BetterBot bot) {
     mBot = bot;
@@ -96,6 +121,13 @@ public class HunterBeastSolver implements RotationSolver {
 
   @Override
   public void combat(Unit u) {
+    // Remove Aspect of the Cheetah / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mAspectOfTheCheetah) || mPlayer.hasAura(mMounts))) {
+      switchActionBar(2);
+      mKeyboard.type('0');
+      switchActionBar(1);
+    }
+
     switchActionBar(1);
 
     // Make sure the pet is attacked and not the player
@@ -159,7 +191,7 @@ public class HunterBeastSolver implements RotationSolver {
 
     // Feign Death
     if (mPlayerLevel >= 30 && u.getTarget() == mPlayer.getGUID() && mPet != null && !mPet.isDead()
-        && !mBot.anyOnCD(mFeignDeath) && targetHealth < 0.85f){
+        && !mBot.anyOnCD(mFeignDeath) && targetHealth < 0.85f) {
       mKeyboard.type('6');
     }
 
@@ -328,6 +360,14 @@ public class HunterBeastSolver implements RotationSolver {
     // Slow dooooown!
     if (now - waitFlag > 500) {
       waitFlag = now;
+
+      // Remove Aspect of the Cheetah / Mount
+      if (mPlayerLevel >= 20 && (mPlayer.hasAura(mAspectOfTheCheetah) || mPlayer.hasAura(mMounts))) {
+        switchActionBar(2);
+        mKeyboard.type('0');
+        switchActionBar(1);
+      }
+
       // Check for buffs
       if (!isFullBuffed()) {
         return;
@@ -412,6 +452,13 @@ public class HunterBeastSolver implements RotationSolver {
 
   @Override
   public boolean beforeInteract() {
+    // Remove Aspect of the Cheetah / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mAspectOfTheCheetah) || mPlayer.hasAura(mMounts))) {
+      switchActionBar(2);
+      mKeyboard.type('0');
+      switchActionBar(1);
+      return true;
+    }
     return false;
   }
 
@@ -454,10 +501,10 @@ public class HunterBeastSolver implements RotationSolver {
 
   @Override
   public boolean prepareForTravel(Vector3f travelTarget) {
-    // Aspect of the Cheetah
-    if (mPlayerLevel >= 20 && !mPlayer.hasAura(mAspectOfTheCheetah)) {
+    // Aspect of the Cheetah / Mount
+    if (mPlayerLevel >= 20 && mPlayer.hasAura(mAspectOfTheCheetah) && mPlayer.hasAura(mMounts)) {
       switchActionBar(2);
-      mKeyboard.type('9');
+      mKeyboard.type('0');
       switchActionBar(1);
     }
     return false;
--- src/rotations/HunterSolver.java
+++ src/rotations/HunterSolver.java
@@ -56,7 +56,32 @@ public class HunterSolver implements RotationSolver {
   int mDrinkingBuffs[] = { 430, 431, 432, 1133, 1135, 1137, 10250, 22734, 24355, 29007 };
 
   // Eating Buffs
-  int mEatingBuffs[] = { 433, 434, 1127, 1129, 1131, 25700, 25886, 28616, 29008, 29073 };
+  int mEatingBuffs[] = { 433, 434, 435, 1127, 1129, 1131, 2639, 6410, 7737, 24005, 25700, 25886, 28616, 29008, 29073 };
+
+  // Mounts
+  int mMounts[] = {
+      // Wolfs
+      1132, 6653, 6654, 23251, 23252, 23250,
+      // Raptors
+      10799, 10796, 8395, 23243, 23242, 23241,
+      // Kodos
+      18989, 18990, 23247, 23248, 23249,
+      // Undead Horses
+      17462, 17464, 17463, 17465, 23246,
+      // Horses
+      472, 6648, 458, 470, 23228, 23227, 23229,
+      // Saber
+      10789, 8394, 10793, 23221, 23219, 23338,
+      // Rams
+      6898, 6777, 6899, 23240, 23239, 23238,
+      // Mechanostrider
+      17454, 10873, 17453, 10969, 23222, 23223, 23225,
+      // PvP - Horde
+      22721, 22722, 22724, 22718, 23509,
+      // PvP - Alliance
+      22717, 22723, 22720, 22719, 23510,
+      // Special (mostly Dungeons)
+      24252, 17450, 24242, 16084, 18991, 18992, 17229 };
 
   public HunterSolver(BetterBot bot) {
     mBot = bot;
@@ -79,6 +104,7 @@ public class HunterSolver implements RotationSolver {
       mUsingBow = false;
     } else {
       System.out.println("Can't tell if gun or bow is equipped");
+      System.exit(1);
     }
   }
 
@@ -94,6 +120,13 @@ public class HunterSolver implements RotationSolver {
 
   @Override
   public void combat(Unit u) {
+    // Remove Aspect of the Cheetah / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mAspectOfTheCheetah) || mPlayer.hasAura(mMounts))) {
+      switchActionBar(2);
+      mKeyboard.type('0');
+      switchActionBar(1);
+    }
+
     switchActionBar(1);
 
     // Make sure the pet is attacked and not the player
@@ -303,6 +336,14 @@ public class HunterSolver implements RotationSolver {
     // Slow dooooown!
     if (now - waitFlag > 500) {
       waitFlag = now;
+
+      // Remove Aspect of the Cheetah / Mount
+      if (mPlayerLevel >= 20 && (mPlayer.hasAura(mAspectOfTheCheetah) || mPlayer.hasAura(mMounts))) {
+        switchActionBar(2);
+        mKeyboard.type('0');
+        switchActionBar(1);
+      }
+
       // Check for buffs
       if (!isFullBuffed()) {
         return;
@@ -387,6 +428,13 @@ public class HunterSolver implements RotationSolver {
 
   @Override
   public boolean beforeInteract() {
+    // Remove Aspect of the Cheetah / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mAspectOfTheCheetah) || mPlayer.hasAura(mMounts))) {
+      switchActionBar(2);
+      mKeyboard.type('0');
+      switchActionBar(1);
+      return true;
+    }
     return false;
   }
 
@@ -429,11 +477,17 @@ public class HunterSolver implements RotationSolver {
 
   @Override
   public boolean prepareForTravel(Vector3f travelTarget) {
-    // Aspect of the Cheetah
-    if (mPlayerLevel >= 20 && !mPlayer.hasAura(mAspectOfTheCheetah)) {
+
+    if (mPlayer.isCasting()) {
+      return true;
+    }
+
+    // Aspect of the Cheetah / Mount
+    if (mPlayerLevel >= 20 && !mPlayer.hasAura(mAspectOfTheCheetah) && !mPlayer.hasAura(mMounts)) {
       switchActionBar(2);
-      mKeyboard.type('9');
+      mKeyboard.type('0');
       switchActionBar(1);
+      return true;
     }
     return false;
   }
