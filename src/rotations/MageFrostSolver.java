5a3406fe0441211ea0b34e32e2633dc74e8da2a1
TheCrux <b.mail93@web.de>
2017-11-02 22:01:24 +0100

    Frost Mage updated with better drinking logic and the new UI

--- src/rotations/MageFrostSolver.java
+++ src/rotations/MageFrostSolver.java
@@ -9,21 +9,22 @@ import com.betterbot.api.pub.Keyboard;
 import com.betterbot.api.pub.Unit;
 import com.betterbot.api.pub.Vector3f;
 import com.betterbot.api.pub.RotationSolver;
+import ui.RotationUI;
 
 /**
  *
  * @author TheCrux
  */
-
-public class MageFrostSolver implements RotationSolver {
+public class MageFrostSolver implements RotationSolver, ICommonSettingFunctions {
 
   BetterBot mBot;
+  RotationUI mUI;
   Keyboard mKeyboard;
   Unit mPlayer;
   Inventory mInventory;
 
-  float mDrinkPercent;
-  float mEatPercent;
+  float mDrinkPercentage;
+  float mEatPercentage;
   boolean mDrinking;
   boolean mEating;
   int mPlayerLevel;
@@ -31,54 +32,56 @@ public class MageFrostSolver implements RotationSolver {
 
   // Everything sorted by rank!
   // Spells
-  int mFrostNova[] = { 122, 865, 6131, 10230 };
-  int mFireBlast[] = { 2136, 2137, 2138, 8412, 8413, 10197, 10199 };
+  int mFrostNova[] = {122, 865, 6131, 10230};
+  int mFireBlast[] = {2136, 2137, 2138, 8412, 8413, 10197, 10199};
   int mCounterspell = 2139;
   int mIceBlock = 11958;
 
   // Buffs
-  int mArcaneIntellect[] = { 1459, 1460, 1461, 10156, 10157 };
-  int mFrostArmor[] = { 168, 7300, 7301, 7302, 7320, 10219, 10220 };
-  int mDampenMagic[] = { 604, 8450, 8451, 10173, 10174 };
-  int mIceBarrier[] = { 11426, 13031, 13032, 13033 };
+  int mArcaneIntellect[] = {1459, 1460, 1461, 10156, 10157};
+  int mFrostArmor[] = {168, 7300, 7301, 7302, 7320, 10219, 10220};
+  int mDampenMagic[] = {604, 8450, 8451, 10173, 10174};
+  int mIceBarrier[] = {11426, 13031, 13032, 13033};
 
   // Drinking Buffs
-  int mDrinkingBuffs[] = { 430, 431, 432, 1133, 1135, 1137, 10250, 22734, 24355, 29007 };
-  int mConjuredWater[] = { 5350, 2288, 2136, 3772, 8077, 8078, 8079 };
+  int mDrinkingBuffs[] = {430, 431, 432, 1133, 1135, 1137, 10250, 22734, 24355, 29007};
+  int mConjuredWater[] = {5350, 2288, 2136, 3772, 8077, 8078, 8079};
 
   // Eating Buffs
-  int mEatingBuffs[] = { 433, 434, 435, 1127, 1129, 1131, 2639, 6410, 7737, 24005, 25700, 25886, 28616, 29008, 29073 };
-  int mConjuredFood[] = { 5349, 1113, 1114, 1487, 8075, 8076, 22895 };
+  int mEatingBuffs[] = {433, 434, 435, 1127, 1129, 1131, 2639, 6410, 7737, 24005, 25700, 25886, 28616, 29008, 29073};
+  int mConjuredFood[] = {5349, 1113, 1114, 1487, 8075, 8076, 22895};
+  int mBuffFood[] = {5004, 5005, 5006, 5007, 10256, 10257, 18229, 18230, 18231, 24800, 24869, 25660};
 
-  int mManaGems[] = { 5514, 5513, 8007, 8008 };
+  int mManaGems[] = {5514, 5513, 8007, 8008};
 
   // Mounts
   int mMounts[] = {
-      // Wolfs
-      1132, 6653, 6654, 23251, 23252, 23250,
-      // Raptors
-      10799, 10796, 8395, 23243, 23242, 23241,
-      // Kodos
-      18989, 18990, 23247, 23248, 23249,
-      // Undead Horses
-      17462, 17464, 17463, 17465, 23246,
-      // Horses
-      472, 6648, 458, 470, 23228, 23227, 23229,
-      // Saber
-      10789, 8394, 10793, 23221, 23219, 23338,
-      // Rams
-      6898, 6777, 6899, 23240, 23239, 23238,
-      // Mechanostrider
-      17454, 10873, 17453, 10969, 23222, 23223, 23225,
-      // PvP - Horde
-      22721, 22722, 22724, 22718, 23509,
-      // PvP - Alliance
-      22717, 22723, 22720, 22719, 23510,
-      // Special (mostly Dungeons)
-      24252, 17450, 24242, 16084, 18991, 18992, 17229 };
+    // Wolfs
+    1132, 6653, 6654, 23251, 23252, 23250,
+    // Raptors
+    10799, 10796, 8395, 23243, 23242, 23241,
+    // Kodos
+    18989, 18990, 23247, 23248, 23249,
+    // Undead Horses
+    17462, 17464, 17463, 17465, 23246,
+    // Horses
+    472, 6648, 458, 470, 23228, 23227, 23229,
+    // Saber
+    10789, 8394, 10793, 23221, 23219, 23338,
+    // Rams
+    6898, 6777, 6899, 23240, 23239, 23238,
+    // Mechanostrider
+    17454, 10873, 17453, 10969, 23222, 23223, 23225,
+    // PvP - Horde
+    22721, 22722, 22724, 22718, 23509,
+    // PvP - Alliance
+    22717, 22723, 22720, 22719, 23510,
+    // Special (mostly Dungeons)
+    24252, 17450, 24242, 16084, 18991, 18992, 17229};
 
   public MageFrostSolver(BetterBot bot) {
     mBot = bot;
+    mUI = new RotationUI(this);
     mPlayer = bot.getPlayer();
     mKeyboard = bot.getKeyboard();
     mPlayerLevel = mPlayer.getLevel();
@@ -87,9 +90,10 @@ public class MageFrostSolver implements RotationSolver {
 
     System.out.println("TheCrux's Frost Mage script started");
 
-    mDrinkPercent = 0.5f; // drink if below 50% mana
-    mEatPercent = 0.6f; // eat if below 60% health
+    mDrinkPercentage = mUI.getDrinkPercentage();
+    mEatPercentage = mUI.getEatPercentage();
     mDrinking = false;
+    mEating = false;
   }
 
   void switchActionBar(int bar) {
@@ -128,7 +132,7 @@ public class MageFrostSolver implements RotationSolver {
 
     // Frost Nova
     if (mPlayerLevel >= 4 && targetDistance <= 11 && targetDistance >= 7 && !mBot.anyOnCD(mFrostNova)
-        && manaValue >= 0.15f) {
+            && manaValue >= 0.15f) {
       mKeyboard.type('s'); // stop casting
       mKeyboard.type('3');
     }
@@ -161,21 +165,24 @@ public class MageFrostSolver implements RotationSolver {
 
   @Override
   public void pull(Unit u) {
-    if (isFullBuffed())
+    if (isFullBuffed()) {
       combat(u);
+    }
   }
 
   @Override
   public boolean combatEnd(Unit u) {
 
+    if (!mDrinking && !mEating && !hasEnoughConjuredItems()) {
+      return true;
+    }
+    
     drinkAndEat();
-    if (mDrinking || mEating)
+    if (mDrinking || mEating) {
       return true;
+    }
 
-    if (!isFullBuffed())
-      return true;
-
-    return false;
+    return !isFullBuffed();
   }
 
   boolean isFullBuffed() {
@@ -210,6 +217,8 @@ public class MageFrostSolver implements RotationSolver {
     return true;
   }
 
+  long mFoodTimer;
+
   void drinkAndEat() {
 
     if (mPlayer.isCasting()) {
@@ -219,42 +228,26 @@ public class MageFrostSolver implements RotationSolver {
     float playerMana = mPlayer.getManaFloat();
     float playerHealth = mPlayer.getHealthFloat();
 
-    if (playerMana < mDrinkPercent)
+    if (playerMana < mDrinkPercentage) {
       mDrinking = true;
+    }
 
-    if (playerHealth < mEatPercent)
+    if (playerHealth < mEatPercentage) {
+      if (!mEating) {
+        mFoodTimer = 0;
+      }
       mEating = true;
-
-    // Conjure some water
-    if (mPlayerLevel >= 4 && mInventory.getItemCount(mConjuredWater) <= 1) {
-      switchActionBar(2);
-      mKeyboard.type('8');
-      return;
-    }
-
-    // Conjure some food
-    if (mPlayerLevel >= 6 && mInventory.getItemCount(mConjuredFood) <= 1) {
-      switchActionBar(2);
-      mKeyboard.type('7');
-      return;
-    }
-
-    // Conjure a mana gem
-    if (mPlayerLevel >= 28 && mInventory.getItemCount(mManaGems) == 0) {
-      switchActionBar(2);
-      mKeyboard.type('6');
-      return;
     }
 
     // Drink
-    if (mDrinking && !mPlayer.hasAura(mDrinkingBuffs) && playerMana < mDrinkPercent + 0.05f) {
+    if (mDrinking && !mPlayer.hasAura(mDrinkingBuffs) && playerMana < mDrinkPercentage + 0.05f) {
       switchActionBar(2);
       mKeyboard.type('0');
       mBot.sleep(1200, 1700); // prevent double drinking
     }
 
     // Eat
-    if (mEating && !mPlayer.hasAura(mEatingBuffs) && playerHealth < mEatPercent + 0.05f) {
+    if (mEating && !mPlayer.hasAura(mEatingBuffs) && !mPlayer.hasAura(mBuffFood) && playerHealth < mEatPercentage + 0.05f) {
       switchActionBar(2);
       mKeyboard.type('9');
       mBot.sleep(1200, 1700); // prevent double eating
@@ -269,12 +262,42 @@ public class MageFrostSolver implements RotationSolver {
     }
 
     if (mEating && playerHealth > 0.9f) {
-      mEating = false; // over 90% health, good enough
-      if (!mDrinking) {
+      if (!mDrinking && (!mPlayer.hasAura(mBuffFood) || ((System.currentTimeMillis() - mFoodTimer) > 10500))) {
         mKeyboard.type('w'); // force to be standing
+        mEating = false; // over 90% health, good enough
         switchActionBar(1);
       }
     }
+  }
+
+  boolean hasEnoughConjuredItems() {
+
+    if (mPlayer.isCasting()) {
+      return false;
+    }
+
+    // Conjure some water
+    if (mPlayerLevel >= 4 && mInventory.getItemCount(mConjuredWater) <= 1) {
+      switchActionBar(2);
+      mKeyboard.type('8');
+      return false;
+    }
+
+    // Conjure some food
+    if (mPlayerLevel >= 6 && mInventory.getItemCount(mConjuredFood) <= 1) {
+      switchActionBar(2);
+      mKeyboard.type('7');
+      return false;
+    }
+
+    // Conjure a mana gem
+    if (mPlayerLevel >= 28 && mInventory.getItemCount(mManaGems) == 0) {
+      switchActionBar(2);
+      mKeyboard.type('6');
+      return false;
+    }
+    switchActionBar(1);
+    return true;
   }
 
   @Override
@@ -308,7 +331,7 @@ public class MageFrostSolver implements RotationSolver {
 
   @Override
   public boolean afterResurrect() {
-    return false;
+    return !combatEnd(null);
   }
 
   @Override
@@ -330,7 +353,7 @@ public class MageFrostSolver implements RotationSolver {
 
   @Override
   public JComponent getUI() {
-    return null;
+    return mUI;
   }
 
   @Override
@@ -345,12 +368,32 @@ public class MageFrostSolver implements RotationSolver {
     }
 
     // Mount
-    if (mPlayerLevel >= 40 && !mPlayer.hasAura(mMounts)) {
+    if (mPlayerLevel >= 40 && !mPlayer.hasAura(mMounts) && mUI.shouldUseMount()) {
       switchActionBar(2);
       mKeyboard.type('-');
       switchActionBar(1);
       return true;
     }
     return false;
+  }
+
+  /**
+   * Called from the UI to set new drink percentages
+   *
+   * @param value The new drinkpercentage 0.0-1.0
+   */
+  @Override
+  public void setDrinkPercentage(float value) {
+    mDrinkPercentage = value;
+  }
+
+  /**
+   * Called from the UI to set new eat percentages
+   *
+   * @param value The new eatpercentage 0.0-1.0
+   */
+  @Override
+  public void setEatPercentage(float value) {
+    mEatPercentage = value;
   }
 }
