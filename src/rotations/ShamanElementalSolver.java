55a30bb9a11ae7ce4f12178dc0f13aa2841f0db0
TheCrux <b.mail93@web.de>
2017-11-07 17:54:06 +0100

    Shaman profiles updated

--- src/rotations/ShamanElementalSolver.java
+++ src/rotations/ShamanElementalSolver.java
@@ -8,79 +8,85 @@ import com.betterbot.api.pub.Vector3f;
 import java.awt.event.KeyEvent;
 import javax.swing.JComponent;
 import com.betterbot.api.pub.RotationSolver;
+import ui.RotationUI;
 
 /**
  * This rotation is for the Elemental spec.
+ *
  * @author TheCrux
  */
-
-public class ShamanElementalSolver implements RotationSolver {
+public class ShamanElementalSolver implements RotationSolver, ICommonSettingFunctions {
 
   BetterBot mBot;
+  RotationUI mUI;
   Keyboard mKeyboard;
   Unit mPlayer;
   int mActionBar;
 
-  float mDrinkPercent;
+  float mDrinkPercentage;
   boolean mDrinking;
   boolean mWeaponBuff;
   int mPlayerLevel;
 
   // Everything sorted by rank!
   // Spells
-  int mFlameShock[] = { 8050, 8052, 8053, 10447, 10448, 29228 };
-  int mEarthShock[] = { 8042, 8044, 8045, 8046, 10412, 10413, 10414 };
+  int mFlameShock[] = {8050, 8052, 8053, 10447, 10448, 29228};
+  int mEarthShock[] = {8042, 8044, 8045, 8046, 10412, 10413, 10414};
 
   // Buffs
-  int mLightningShield[] = { 324, 325, 905, 945, 8134, 10431, 10432 };
+  int mLightningShield[] = {324, 325, 905, 945, 8134, 10431, 10432};
   int mGhostWolf = 2645;
 
   // Totem Buffs
-  int mStoneskinTotem[] = { 8071, 8154, 8155, 10406, 10407, 10408 };
-  int mHealingStreamTotem[] = { 5672, 6371, 6372, 10460, 10461 };
+  int mStoneskinTotem[] = {8071, 8154, 8155, 10406, 10407, 10408};
+  int mHealingStreamTotem[] = {5672, 6371, 6372, 10460, 10461};
 
   // Totem Names
-  String mSearingTotem[] = { "Searing Totem", "Searing Totem II", "Searing Totem III", "Searing Totem IV",
-      "Searing Totem V", "Searing Totem VI" };
+  String mSearingTotem[] = {"Searing Totem", "Searing Totem II", "Searing Totem III", "Searing Totem IV",
+    "Searing Totem V", "Searing Totem VI"};
 
   // Drinking Buffs
-  int mDrinkingBuffs[] = { 430, 431, 432, 1133, 1135, 1137, 10250, 22734, 24355, 29007 };
+  int mDrinkingBuffs[] = {430, 431, 432, 1133, 1135, 1137, 10250, 22734, 24355, 29007};
 
   // Mounts
   int mMounts[] = {
-      // Wolfs
-      1132, 6653, 6654, 23251, 23252, 23250,
-      // Raptors
-      10799, 10796, 8395, 23243, 23242, 23241,
-      // Kodos
-      18989, 18990, 23247, 23248, 23249,
-      // Undead Horses
-      17462, 17464, 17463, 17465, 23246,
-      // Horses
-      472, 6648, 458, 470, 23228, 23227, 23229,
-      // Saber
-      10789, 8394, 10793, 23221, 23219, 23338,
-      // Rams
-      6898, 6777, 6899, 23240, 23239, 23238,
-      // Mechanostrider
-      17454, 10873, 17453, 10969, 23222, 23223, 23225,
-      // PvP - Horde
-      22721, 22722, 22724, 22718, 23509,
-      // PvP - Alliance
-      22717, 22723, 22720, 22719, 23510,
-      // Special (mostly Dungeons)
-      24252, 17450, 24242, 16084, 18991, 18992, 17229 };
+    // Wolfs
+    1132, 6653, 6654, 23251, 23252, 23250,
+    // Raptors
+    10799, 10796, 8395, 23243, 23242, 23241,
+    // Kodos
+    18989, 18990, 23247, 23248, 23249,
+    // Undead Horses
+    17462, 17464, 17463, 17465, 23246,
+    // Horses
+    472, 6648, 458, 470, 23228, 23227, 23229,
+    // Saber
+    10789, 8394, 10793, 23221, 23219, 23338,
+    // Rams
+    6898, 6777, 6899, 23240, 23239, 23238,
+    // Mechanostrider
+    17454, 10873, 17453, 10969, 23222, 23223, 23225,
+    // PvP - Horde
+    22721, 22722, 22724, 22718, 23509,
+    // PvP - Alliance
+    22717, 22723, 22720, 22719, 23510,
+    // Special (mostly Dungeons)
+    24252, 17450, 24242, 16084, 18991, 18992, 17229};
 
   public ShamanElementalSolver(BetterBot bot) {
     mBot = bot;
+    mUI = new RotationUI(this);
     mPlayer = bot.getPlayer();
     mKeyboard = bot.getKeyboard();
     mPlayerLevel = mPlayer.getLevel();
     mActionBar = 1;
 
+    mUI.removeMountSettings();
+    mUI.removeEatingSettings();
+
     System.out.println("TheCrux's Elemental Shaman script started");
 
-    mDrinkPercent = 0.4f; // drink if below 40% mana
+    mDrinkPercentage = mUI.getDrinkPercentage();
     mDrinking = false;
     mWeaponBuff = false;
   }
@@ -98,18 +104,12 @@ public class ShamanElementalSolver implements RotationSolver {
   @Override
   public void combat(Unit u) {
 
-    // Remove Ghost Wolf
-    if (mPlayerLevel >= 20 && mPlayerLevel < 40 && mPlayer.hasAura(mGhostWolf)) {
-      switchActionBar(2);
-      mKeyboard.type('9');
-      switchActionBar(1);
-      return;
-    }
-    // Remove Mount
-    else if (mPlayerLevel >= 40 && mPlayer.hasAura(mMounts)) {
+    // Remove Ghost Wolf / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mGhostWolf) || mPlayer.hasAura(mMounts))) {
       switchActionBar(2);
       mKeyboard.type('0');
       switchActionBar(1);
+      return;
     }
 
     if (mPlayer.isCasting()) {
@@ -128,11 +128,11 @@ public class ShamanElementalSolver implements RotationSolver {
         // Flame Shock
         if (mPlayerLevel >= 10 && targetHealth >= 0.3f) {
           if (!u.hasAura(mFlameShock) && !mBot.anyOnCD(mFlameShock) && !mBot.anyOnCD(mEarthShock)
-              && targetHealth >= 0.15f && manaValue >= 0.15f) {
+                  && targetHealth >= 0.15f && manaValue >= 0.15f) {
             mKeyboard.type('2');
           }
           // Searing Totem
-          else if (mBot.getNPCs(mSearingTotem).size() == 0) {
+          else if (mBot.getNPCs(mSearingTotem).isEmpty()) {
             mKeyboard.type('9');
           }
           // Earth Shock
@@ -147,7 +147,7 @@ public class ShamanElementalSolver implements RotationSolver {
       }
 
       // Chain Lightning
-      if (mBot.getAttackers().size() > 2 && manaValue >= 0.2f) {
+      if (mPlayerLevel >= 32 && mBot.getAttackers().size() > 2 && manaValue >= 0.2f) {
         mKeyboard.type('5');
       }
       // Lightning Bolt
@@ -170,7 +170,7 @@ public class ShamanElementalSolver implements RotationSolver {
 
   @Override
   public void pull(Unit u) {
-    if (mPlayer.getManaFloat() < mDrinkPercent) {
+    if (mPlayer.getManaFloat() < mDrinkPercentage) {
       drink();
     }
     // Heal yourself
@@ -178,7 +178,7 @@ public class ShamanElementalSolver implements RotationSolver {
       mKeyboard.type('3');
     }
     // Searing Totem
-    else if (mBot.getNPCs(mSearingTotem).size() == 0) {
+    else if (mBot.getNPCs(mSearingTotem).isEmpty()) {
       mKeyboard.type('9');
     }
     // Buff up
@@ -191,7 +191,21 @@ public class ShamanElementalSolver implements RotationSolver {
   public boolean combatEnd(Unit u) {
 
     drink();
-    return mDrinking;
+    if (mDrinking) {
+      return true;
+    }
+
+    if (mPlayer.isCasting()) {
+      return true;
+    }
+
+    // Healing Wave
+    if (mPlayer.getHealthFloat() < 0.7f) {
+      mKeyboard.type('3');
+      return true;
+    }
+
+    return !isFullBuffed(40);
   }
 
   boolean isFullBuffed(float distToTarget) {
@@ -207,20 +221,30 @@ public class ShamanElementalSolver implements RotationSolver {
       mKeyboard.type('6');
       return false;
     }
+
+    if (distToTarget < 35) {
+      return totemsPlaced();
+    }
+
+    return true;
+  }
+
+  boolean totemsPlaced() {
+
     // Stoneskin Totem
-    if (mPlayerLevel >= 4 && distToTarget < 20 && !mPlayer.hasAura(mStoneskinTotem) && mPlayer.getManaFloat() > 0.15f) {
+    if (mPlayerLevel >= 4 && !mPlayer.hasAura(mStoneskinTotem) && mPlayer.getManaFloat() > 0.15f) {
       mKeyboard.type('8');
       return false;
     }
-
     return true;
   }
 
   void drink() {
     float manaValue = mPlayer.getManaFloat();
 
-    if (manaValue < mDrinkPercent)
+    if (manaValue < mDrinkPercentage) {
       mDrinking = true;
+    }
 
     if (mDrinking && !mPlayer.hasAura(mDrinkingBuffs)) {
       mKeyboard.type('0'); // drink bind
@@ -249,14 +273,8 @@ public class ShamanElementalSolver implements RotationSolver {
     // Slow dooooown!
     if (now - waitFlag > 500) {
 
-      // Remove Ghost Wolf
-      if (mPlayerLevel >= 20 && mPlayerLevel < 40 && mPlayer.hasAura(mGhostWolf)) {
-        switchActionBar(2);
-        mKeyboard.type('0');
-        switchActionBar(1);
-      }
-      // Remove Mount
-      else if (mPlayerLevel >= 40 && mPlayer.hasAura(mMounts)) {
+      // Remove Ghost Wolf / Mount
+      if (mPlayerLevel >= 20 && (mPlayer.hasAura(mGhostWolf) || mPlayer.hasAura(mMounts))) {
         switchActionBar(2);
         mKeyboard.type('0');
         switchActionBar(1);
@@ -268,12 +286,7 @@ public class ShamanElementalSolver implements RotationSolver {
 
   @Override
   public boolean afterResurrect() {
-
-    if (mPlayer.inCombat())
-      return false;
-
-    drink();
-    return mDrinking;
+    return combatEnd(null);
   }
 
   @Override
@@ -284,15 +297,8 @@ public class ShamanElementalSolver implements RotationSolver {
   @Override
   public boolean beforeInteract() {
 
-    // Remove Ghost Wolf
-    if (mPlayerLevel >= 20 && mPlayerLevel < 40 && mPlayer.hasAura(mGhostWolf)) {
-      switchActionBar(2);
-      mKeyboard.type('0');
-      switchActionBar(1);
-      return true;
-    }
-    // Remove Mount
-    else if (mPlayerLevel >= 40 && mPlayer.hasAura(mMounts)) {
+    // Remove Ghost Wolf / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mGhostWolf) || mPlayer.hasAura(mMounts))) {
       switchActionBar(2);
       mKeyboard.type('0');
       switchActionBar(1);
@@ -303,7 +309,7 @@ public class ShamanElementalSolver implements RotationSolver {
 
   @Override
   public JComponent getUI() {
-    return null;
+    return mUI;
   }
 
   @Override
@@ -317,20 +323,22 @@ public class ShamanElementalSolver implements RotationSolver {
       return true;
     }
 
-    // Ghost Wolf
-    if (mPlayerLevel >= 20 && mPlayerLevel < 40 && !mPlayer.hasAura(mGhostWolf)) {
-      switchActionBar(2);
-      mKeyboard.type('9');
-      switchActionBar(1);
-      return true;
-    }
-    // Mount
-    else if (mPlayerLevel >= 40 && !mPlayer.hasAura(mMounts)) {
+    // Ghost Wolf / Mount
+    if (mPlayerLevel >= 20 && !mPlayer.hasAura(mGhostWolf) && !mPlayer.hasAura(mMounts)) {
       switchActionBar(2);
       mKeyboard.type('0');
       switchActionBar(1);
       return true;
     }
     return false;
+  }
+
+  @Override
+  public void setDrinkPercentage(float value) {
+    mDrinkPercentage = value;
+  }
+
+  @Override
+  public void setEatPercentage(float value) {
   }
 }
--- src/rotations/ShamanEnhancementSolver.java
+++ src/rotations/ShamanEnhancementSolver.java
@@ -5,49 +5,54 @@ import com.betterbot.api.pub.Database.Vendor;
 import com.betterbot.api.pub.Keyboard;
 import com.betterbot.api.pub.Unit;
 import com.betterbot.api.pub.Vector3f;
-import java.awt.event.KeyEvent;
-import javax.swing.JComponent;
 import com.betterbot.api.pub.RotationSolver;
 
+import java.awt.event.KeyEvent;
+import javax.swing.JComponent;
+import ui.RotationUI;
+
 /**
- * This rotation is for the Enhancement spec. You have to skill Stormstrike on level 40!
+ * This rotation is for the Enhancement spec. You have to skill Stormstrike on
+ * level 40!
+ *
  * @author TheCrux
  */
-
-public class ShamanEnhancementSolver implements RotationSolver {
+public class ShamanEnhancementSolver implements RotationSolver, ICommonSettingFunctions {
 
   BetterBot mBot;
   Keyboard mKeyboard;
   Unit mPlayer;
+  RotationUI mUI;
   int mActionBar;
 
-  float mDrinkPercent;
+  float mDrinkPercentage;
   boolean mDrinking;
   boolean mWeaponBuff;
   int mPlayerLevel;
 
   // Everything sorted by rank!
   // Spells
-  int mFlameShock[] = { 8050, 8052, 8053, 10447, 10448, 29228 };
-  int mEarthShock[] = { 8042, 8044, 8045, 8046, 10412, 10413, 10414 };
+  int mFlameShock[] = {8050, 8052, 8053, 10447, 10448, 29228};
+  int mEarthShock[] = {8042, 8044, 8045, 8046, 10412, 10413, 10414};
   int mStormstrike = 17364;
 
   // Buffs
-  int mLightningShield[] = { 324, 325, 905, 945, 8134, 10431, 10432 };
+  int mLightningShield[] = {324, 325, 905, 945, 8134, 10431, 10432};
   int mGhostWolf = 2645;
 
   // Totem Buffs
-  int mStrengthOfEarthTotem[] = { 8076, 8162, 8163, 10441, 25362 };
+  int mStrengthOfEarthTotem[] = {8076, 8162, 8163, 10441, 25362};
+  int mGraceOfAirTotem[] = {8836, 10626, 25360};
 
   // Totem Names
-  String mSearingTotem[] = { "Searing Totem", "Searing Totem II", "Searing Totem III", "Searing Totem IV",
-      "Searing Totem V", "Searing Totem VI" };
+  String mSearingTotem[] = {"Searing Totem", "Searing Totem II", "Searing Totem III", "Searing Totem IV",
+    "Searing Totem V", "Searing Totem VI"};
 
   // Drinking Buffs
-  int mDrinkingBuffs[] = { 430, 431, 432, 1133, 1135, 1137, 10250, 22734, 24355, 29007 };
+  int mDrinkingBuffs[] = {430, 431, 432, 1133, 1135, 1137, 10250, 22734, 24355, 29007};
 
-	// Mounts
-	int mMounts[] = {
+  // Mounts
+  int mMounts[] = {
     // Wolfs
     1132, 6653, 6654, 23251, 23252, 23250,
     // Raptors
@@ -69,18 +74,22 @@ public class ShamanEnhancementSolver implements RotationSolver {
     // PvP - Alliance
     22717, 22723, 22720, 22719, 23510,
     // Special (mostly Dungeons)
-    24252, 17450, 24242, 16084, 18991, 18992, 17229 };
+    24252, 17450, 24242, 16084, 18991, 18992, 17229};
 
   public ShamanEnhancementSolver(BetterBot bot) {
     mBot = bot;
+    mUI = new RotationUI(this);
     mPlayer = bot.getPlayer();
     mKeyboard = bot.getKeyboard();
     mPlayerLevel = mPlayer.getLevel();
     mActionBar = 1;
 
+    mUI.removeMountSettings();
+    mUI.removeEatingSettings();
+
     System.out.println("TheCrux's Enhancement Shaman script started");
 
-    mDrinkPercent = 0.4f; // drink if below 40% mana
+    mDrinkPercentage = mUI.getDrinkPercentage();
     mDrinking = false;
     mWeaponBuff = false;
   }
@@ -98,18 +107,12 @@ public class ShamanEnhancementSolver implements RotationSolver {
   @Override
   public void combat(Unit u) {
 
-    // Remove Ghost Wolf
-    if (mPlayerLevel >= 20 && mPlayerLevel < 40 && mPlayer.hasAura(mGhostWolf)) {
-      switchActionBar(2);
-      mKeyboard.type('9');
-      switchActionBar(1);
-      return;
-    }
-    // Remove Mount
-    else if (mPlayerLevel >= 40 && mPlayer.hasAura(mMounts)) {
+    // Remove Ghost Wolf / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mGhostWolf) || mPlayer.hasAura(mMounts))) {
       switchActionBar(2);
       mKeyboard.type('0');
       switchActionBar(1);
+      return;
     }
 
     if (mPlayer.isCasting()) {
@@ -127,15 +130,23 @@ public class ShamanEnhancementSolver implements RotationSolver {
       if (targetDistance < 20) {
 
         // Make sure Strength of Earth Totem is up
-        if (!mPlayer.hasAura(mStrengthOfEarthTotem) && targetDistance <= 10 && targetHealth > 0.3f
-            && manaValue > 0.15f) {
+        if (mPlayerLevel >= 10 && !mPlayer.hasAura(mStrengthOfEarthTotem) && targetDistance <= 10 && targetHealth > 0.3f
+                && manaValue > 0.15f) {
+          switchActionBar(2);
           mKeyboard.type('8');
+          switchActionBar(1);
+        }
+        // Make sure Grace of Air Totem is up
+        else if (mPlayerLevel >= 42 && !mPlayer.hasAura(mGraceOfAirTotem) && targetDistance <= 10 && targetHealth > 0.3f && manaValue > 0.15f) {
+          switchActionBar(2);
+          mKeyboard.type('7');
+          switchActionBar(1);
         }
 
         if (targetDistance <= 5) {
           // Stormstrike
           if (mPlayerLevel >= 40 && targetDistance <= 5 && !mBot.anyOnCD(mStormstrike) && targetHealth > 0.1f
-              && manaValue > 0.3f) {
+                  && manaValue > 0.3f) {
             mKeyboard.type('5');
           }
           // Auto Attack
@@ -147,20 +158,22 @@ public class ShamanEnhancementSolver implements RotationSolver {
         if (mPlayerLevel >= 10 && targetHealth > 0.3f && manaValue > 0.15f) {
           // Flame Shock
           if (!u.hasAura(mFlameShock) && !mBot.anyOnCD(mFlameShock) && !mBot.anyOnCD(mEarthShock)
-              && (mPlayerLevel < 40 || (mBot.anyOnCD(mStormstrike) && !u.hasAura(mStormstrike)))) {
+                  && (mPlayerLevel < 40 || (mBot.anyOnCD(mStormstrike) && !u.hasAura(mStormstrike)))) {
             mKeyboard.type('2');
           }
           // Searing Totem
-          else if (mBot.getNPCs(mSearingTotem).size() == 0 && targetHealth > 0.5f) {
+          else if (mBot.getNPCs(mSearingTotem).isEmpty() && targetHealth > 0.5f) {
             mKeyboard.type('9');
           }
         }
         // Earth Shock
         if (mPlayerLevel >= 4 && !mBot.anyOnCD(mEarthShock) && manaValue > 0.15f) {
-          if (mPlayerLevel >= 10 && mPlayerLevel < 40 && mBot.anyOnCD(mFlameShock))
+          if (mPlayerLevel >= 10 && mPlayerLevel < 40 && mBot.anyOnCD(mFlameShock)) {
             return;
-          if (mPlayerLevel >= 40 && !u.hasAura(mStormstrike))
+          }
+          if (mPlayerLevel >= 40 && !u.hasAura(mStormstrike)) {
             return;
+          }
           mKeyboard.type('1');
         }
       }
@@ -180,27 +193,43 @@ public class ShamanEnhancementSolver implements RotationSolver {
 
   @Override
   public void pull(Unit u) {
-    if (mPlayer.getManaFloat() < mDrinkPercent) {
-      drink();
+    // Remove Ghost Wolf / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mGhostWolf) || mPlayer.hasAura(mMounts))) {
+      switchActionBar(2);
+      mKeyboard.type('0');
+      switchActionBar(1);
+      return;
     }
-    // Heal yourself
-    else if (mPlayer.getHealthFloat() < 0.85f) {
-      mKeyboard.type('3');
-    }
+
     // Searing Totem
-    else if (mBot.getNPCs(mSearingTotem).size() == 0) {
+    if (mPlayerLevel >= 10 && mBot.getNPCs(mSearingTotem).isEmpty()) {
       mKeyboard.type('9');
     }
     // Buff up
-    else if (isFullBuffed(u.getDistance()))
+    else if (isFullBuffed(u.getDistance())) {
       combat(u);
+    }
   }
 
   @Override
   public boolean combatEnd(Unit u) {
-
     drink();
-    return mDrinking;
+
+    if (mDrinking) {
+      return true;
+    }
+
+    if (mPlayer.isCasting()) {
+      return true;
+    }
+
+    // Healing Wave
+    if (mPlayer.getHealthFloat() < 0.7f) {
+      mKeyboard.type('3');
+      return true;
+    }
+
+    return !isFullBuffed(40);
   }
 
   boolean isFullBuffed(float distToTarget) {
@@ -211,26 +240,48 @@ public class ShamanEnhancementSolver implements RotationSolver {
       mWeaponBuff = true;
       return false;
     }
+
+    float manaValue = mPlayer.getManaFloat();
+
     // Lightning Shield
-    if (mPlayerLevel >= 8 && !mPlayer.hasAura(mLightningShield) && mPlayer.getManaFloat() > 0.15f) {
+    if (mPlayerLevel >= 8 && !mPlayer.hasAura(mLightningShield) && manaValue > 0.15f) {
       mKeyboard.type('6');
       return false;
     }
-    // Strength of Earth Totem
-    if (mPlayerLevel >= 10 && distToTarget < 35 && !mPlayer.hasAura(mStrengthOfEarthTotem)
-        && mPlayer.getManaFloat() > 0.15f) {
-      mKeyboard.type('8');
-      return false;
+
+    if (distToTarget < 35) {
+      return totemsPlaced();
     }
 
+    return true;
+  }
+
+  boolean totemsPlaced() {
+    float manaValue = mPlayer.getManaFloat();
+
+    // Strength of Earth Totem
+    if (mPlayerLevel >= 10 && !mPlayer.hasAura(mStrengthOfEarthTotem)
+            && manaValue > 0.15f) {
+      switchActionBar(2);
+      mKeyboard.type('8');
+      switchActionBar(1);
+      return false;
+    }
+    // Grace of Air Totem
+    if (mPlayerLevel >= 42 && !mPlayer.hasAura(mGraceOfAirTotem) && manaValue > 0.15f) {
+      switchActionBar(2);
+      mKeyboard.type('7');
+      switchActionBar(1);
+    }
     return true;
   }
 
   void drink() {
     float manaValue = mPlayer.getManaFloat();
 
-    if (manaValue < mDrinkPercent)
+    if (manaValue < mDrinkPercentage) {
       mDrinking = true;
+    }
 
     if (mDrinking && !mPlayer.hasAura(mDrinkingBuffs)) {
       mKeyboard.type('0'); // drink bind
@@ -258,18 +309,12 @@ public class ShamanEnhancementSolver implements RotationSolver {
     long now = System.currentTimeMillis();
     // Slow dooooown!
     if (now - waitFlag > 500) {
-      // Remove Ghost Wolf
-      if (mPlayerLevel >= 20 && mPlayerLevel < 40 && mPlayer.hasAura(mGhostWolf)) {
-        switchActionBar(2);
-        mKeyboard.type('9');
-        switchActionBar(1);
-        return;
-      }
-      // Remove Mount
-      else if (mPlayerLevel >= 40 && mPlayer.hasAura(mMounts)) {
+      // Remove Ghost Wolf / Mount
+      if (mPlayerLevel >= 20 && (mPlayer.hasAura(mGhostWolf) || mPlayer.hasAura(mMounts))) {
         switchActionBar(2);
         mKeyboard.type('0');
         switchActionBar(1);
+        return;
       }
       waitFlag = now;
       isFullBuffed(u.getDistance());
@@ -278,12 +323,7 @@ public class ShamanEnhancementSolver implements RotationSolver {
 
   @Override
   public boolean afterResurrect() {
-
-    if (mPlayer.inCombat())
-      return false;
-
-    drink();
-    return mDrinking;
+    return combatEnd(null);
   }
 
   @Override
@@ -293,15 +333,8 @@ public class ShamanEnhancementSolver implements RotationSolver {
 
   @Override
   public boolean beforeInteract() {
-    // Remove Ghost Wolf
-    if (mPlayerLevel >= 20 && mPlayerLevel < 40 && mPlayer.hasAura(mGhostWolf)) {
-      switchActionBar(2);
-      mKeyboard.type('9');
-      switchActionBar(1);
-      return true;
-    }
-    // Remove Mount
-    else if (mPlayerLevel >= 40 && mPlayer.hasAura(mMounts)) {
+    // Remove Ghost Wolf / Mount
+    if (mPlayerLevel >= 20 && (mPlayer.hasAura(mGhostWolf) || mPlayer.hasAura(mMounts))) {
       switchActionBar(2);
       mKeyboard.type('0');
       switchActionBar(1);
@@ -312,7 +345,7 @@ public class ShamanEnhancementSolver implements RotationSolver {
 
   @Override
   public JComponent getUI() {
-    return null;
+    return mUI;
   }
 
   @Override
@@ -326,20 +359,22 @@ public class ShamanEnhancementSolver implements RotationSolver {
       return true;
     }
 
-    // Ghost Wolf
-    if (mPlayerLevel >= 20 && mPlayerLevel < 40 && !mPlayer.hasAura(mGhostWolf)) {
-      switchActionBar(2);
-      mKeyboard.type('9');
-      switchActionBar(1);
-      return true;
-    }
-    // Mount
-    else if (mPlayerLevel >= 40 && !mPlayer.hasAura(mMounts)) {
+    // Ghost Wolf / Mount
+    if (mPlayerLevel >= 20 && !mPlayer.hasAura(mGhostWolf) && !mPlayer.hasAura(mMounts)) {
       switchActionBar(2);
       mKeyboard.type('0');
       switchActionBar(1);
       return true;
     }
     return false;
+  }
+
+  @Override
+  public void setDrinkPercentage(float value) {
+    mDrinkPercentage = value;
+  }
+
+  @Override
+  public void setEatPercentage(float value) {
   }
 }
